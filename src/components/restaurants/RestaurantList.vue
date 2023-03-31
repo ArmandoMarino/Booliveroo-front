c98d72fda178a1eb7a6f03b520633b49d5c697b3
MindulaHewadewage <mindufernando@gmail.com>
2023-03-31 15:28:13 +0200

    restaurant list

---
+++ src/components/restaurants/RestaurantList.vue
@@ -0,0 +1,34 @@
+<script>
+import RestaurantCard from './RestaurantCard.vue';
+export default {
+    name: 'RestaurantList',
+    components: { RestaurantCard },
+    props: {
+        restaurants: Object,
+    },
+
+}
+</script>
+
+
+<template>
+    <div class="restaurant-list">
+        <div class="container">
+            <section class="restaurants">
+                <h4>Lista Ristoranti</h4>
+                <div class="row">
+                    <div class="col-6 col-md-4 col-lg-3 col-xl-2">
+                        <restaurant-card v-for="restaurant in restaurants" :key="restaurant.name"
+                            v-bind="restaurant"></restaurant-card>
+                    </div>
+
+                </div>
+            </section>
+            <section class="row new"></section>
+            <section class="row top-rated"></section>
+        </div>
+    </div>
+</template>
+
+
+<style></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,30 @@
 <script>
+import axios from "axios";
 import AppMain from "../components/AppMain.vue"
+import RestaurantList from "../components/restaurants/RestaurantList.vue";
 export default {
+    data() {
+        return {
+            restaurants: []
+        }
+    },
     name: 'HomePage',
-    components: { AppMain }
+    components: { AppMain, RestaurantList },
+    methods: {
+        fetchRestaurants() {
+            axios.get('http://localhost:3000/restaurants')
+                .then(res => { console.log(res); this.restaurants = res.data; });
+        }
+    },
+    created() {
+        this.fetchRestaurants();
+    }
 }
 </script>
 
 <template>
-    <app-main></app-main>
+    <!-- <app-main></app-main> -->
+    <restaurant-list></restaurant-list>
 </template>
 
 <style></style>
