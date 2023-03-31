947745346aaa609f549818c2ddbddd0e3a148744
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-31 13:14:02 +0200

    card adapting

--- src/components/restaurants/RestaurantCard.vue
+++ src/components/restaurants/RestaurantCard.vue
@@ -2,52 +2,26 @@
 export default {
     name: "RestaurantCard",
     props: {
-        restourant: Object,
+        restaurant: Object,
         isDetail: Boolean,
     },
     computed: {
-        gameDateUpdated() {
-            const date = new Date(this.game.updated_at);
-            let day = date.getDay();
-            let month = date.getMonth();
-            const year = date.getFullYear();
-            const hours = date.getHours();
-            const minutes = date.getMinutes();
-            const seconds = date.getSeconds();
-
-            // Fix zero bug
-            if (day < 10) day = "0" + day;
-            if (month < 10) month = "0" + month;
-
-            return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds} `;
-        },
-        gameDateCreated() {
-            const date = new Date(this.game.created_at);
-            let day = date.getDay();
-            let month = date.getMonth();
-            const year = date.getFullYear();
-            const hours = date.getHours();
-            const minutes = date.getMinutes();
-            const seconds = date.getSeconds();
-
-            // Fix zero bug
-            if (day < 10) day = "0" + day;
-            if (month < 10) month = "0" + month;
-
-            return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds} `;
-        },
-        abstractDescription() {
-            const abstract = this.game.description.slice(0, 70);
-            return abstract + "...";
-        },
         setVote() {
-            let baseVote = this.game.vote;
-            const transformedVote = ((baseVote - 1) * 4) / 9 + 1;
-            const finalVote = Math.floor(transformedVote);
-            return finalVote;
-        },
-    },
-};
+            let baseVote = this.restaurant.vote;
+            const finalVote = Math.floor(baseVote);
+
+            if (finalVote >= 0 && finalVote <= 3) {
+                return finalVote;
+            } else {
+                if (finalVote >= 3 && finalVote <= 4) {
+                    return finalVote + " " + "Ottimo";
+                } else {
+                    return finalVote + " " + "Eccellente";
+                }
+            }
+        }
+    }
+}
 </script>
 
 
@@ -58,12 +32,17 @@ export default {
     <div class="col-lg-3 col-md-6 col-sm-10 my-card text-center bounce my-4">
         <div class="card">
             <img :src="restaurant.image" class="card-img-top" :alt="restaurant.name">
-            <div class="card-body">
+            <div class="card-body p-2 text-start">
                 <p><strong>{{ restaurant.food }}</strong></p>
-                <p>Vote</p>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
+                <p>{{ setVote }}</p>
+                <!-- <p>{{ restaurant.distance }}</p> -->
             </div>
         </div>
     </div>
 </template>
+
+<style scoped lang="scss">
+.card-body p {
+    margin: 0;
+}
+</style>
