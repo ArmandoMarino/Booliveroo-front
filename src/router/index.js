8f0188ccd407a3630a8444e893b1105774731666
AlessandroLembo <alessandrolembo2@gmail.com>
2023-04-03 14:54:16 +0200

    create route for About Us page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import HomePage from "../pages/HomePage.vue";
 import RestaurantPage from "../pages/restaurants/RestaurantPage.vue"
 import NotFoundPage from "../pages/NotFoundPage.vue";
 import ContactPage from "../pages/ContactPage.vue";
+import AboutUsPage from "../pages/AboutUsPage";
 
 // Creo il Router e definisco le rotte
 const router = createRouter({
@@ -18,6 +19,9 @@ const router = createRouter({
     
     // Rotta per la pagina del form
     { path: '/contact', name: 'contact_page', component: ContactPage},
+    
+    // Rotta per la pagina About us
+    { path: '/about_us', name: 'about_us', component: AboutUsPage},
 
     { path: "/restaurants", name: "restaurants", component: RestaurantPage },
 
