8f9c2a8e1df4b12b568b5b26af25d8f9f063bec0
AlessandroLembo <alessandrolembo2@gmail.com>
2023-04-03 10:49:52 +0200

    first commit branch feature-contactpage

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // Importo le pagine che voglio usare
 import HomePage from "../pages/HomePage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
+import ContactPage from '../pages/ContactPage';
 
 // Creo il Router e definisco le rotte
 const router = createRouter({
@@ -13,6 +14,9 @@ const router = createRouter({
   // Rotte che useremo
   routes: [
     { path: "/", name: "home", component: HomePage },
+
+    // Rotta per la pagina del form
+    {path: '/contact', name: 'contact_page', component: ContactPage},
 
     // NotFoundPage (lasciatela come ultima posizione )
     { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
