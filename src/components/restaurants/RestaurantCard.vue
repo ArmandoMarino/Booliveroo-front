a89b50c3cc904d75eeadb337d0e0d8b64f541160
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-04-06 11:57:55 +0200

    fix card

--- src/components/restaurants/RestaurantCard.vue
+++ src/components/restaurants/RestaurantCard.vue
@@ -136,12 +136,13 @@ export default {
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="modal-cart-Label">Modal title</h1>
+                                        <h1 class="modal-title fs-5" id="modal-cart-Label">{{ food.label }}</h1>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                                             aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        <p>{{ food.name }}</p>
+                                        <!-- TODO IMG FOOD -->
+                                        <p>{{ food.description }}</p>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary"
