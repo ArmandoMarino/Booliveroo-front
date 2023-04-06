08cc928132c5b36f2cbbc15470bda5f219cfc15a
AlessandroLembo <alessandrolembo2@gmail.com>
2023-04-06 15:15:10 +0200

    changes style at checkbox list

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
+import SideBar from "./components/SideBar.vue";
 
 import axios from "axios";
 export default {
   name: "App",
-  components: { AppHeader, AppFooter }
+  components: { AppHeader, AppFooter, SideBar }
 }
 </script>
 
 
 <template>
   <app-header></app-header>
-  <RouterView />
+  <!-- <RouterView /> -->
+  <side-bar></side-bar>
   <app-footer></app-footer>
 </template>
 
