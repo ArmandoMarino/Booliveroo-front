634862f50cff6f085f15b48fb2546a8d260793f9
MindulaHewadewage <mindufernando@gmail.com>
2023-04-06 11:05:30 +0200

    chiamata axios delle categorie

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,29 @@
 <script>
+import axios from 'axios';
+const apiBase = 'http://127.0.0.1:8000/api/restaurants';
 import AppMain from "../components/AppMain.vue"
 export default {
     name: 'HomePage',
+    data() {
+        return {
+            restaurants: {}
+        }
+    },
     components: { AppMain },
+
+    methods: {
+        fetchRestaurants() {
+            axios.get(apiBase).then(res => { this.restaurants = res.data })
+        }
+    },
+    created() {
+        this.fetchRestaurants();
+    }
 }
 </script>
 
 <template>
-    <app-main></app-main>
+    <app-main :restaurants="restaurants"></app-main>
 </template>
 
 <style></style>
