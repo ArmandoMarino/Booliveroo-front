28558f621041ccee8cc23738a43689c60af04a59
AlessandroLembo <alessandrolembo2@gmail.com>
2023-04-13 17:33:19 +0200

    rename component

---
+++ src/pages/WorkInProgressPage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "WorkInProgress",
+};
+</script>
+
+<template>
+    <div>
+        <h1>work in progress</h1>
+    </div>
+    <button class="btn btn-sm btn-secondary" @click="$router.back()">
+        Torna Indietro
+    </button>
+</template>
+
+<style scoped lang="scss"></style>
