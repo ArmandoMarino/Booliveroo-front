f6318d92feebde2138cd1ddfd80505bd0777ebe6
MindulaHewadewage <mindufernando@gmail.com>
2023-04-13 23:43:03 +0200

    fixed border-radius

--- src/data/images.js
+++ src/data/images.js
@@ -1,54 +1,52 @@
-const sliderImages = {
-  images: [
-    {
-      type: "rect",
-      url: "https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg",
-    },
-    {
-      type: "square",
-      url: "https://www.diregiovani.it/wp-content/uploads/2017/02/logo-McDonald%E2%80%99s.jpg",
-    },
-    {
-      type: "square",
-      url: "https://media.cdnandroid.com/item_images/600470/imagen-old-wild-west-0ori.jpg",
-    },
-    {
-      type: "square",
-      url: "https://play-lh.googleusercontent.com/uBCQPdCCHbFJCk3dDidGQ1paIeHaLRi-tJn83t2MkRoYiUX0X3K9CP3uV7dusjfRsqV5",
-    },
-    {
-      type: "rect",
-      url: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg",
-    },
-    {
-      type: "rect",
-      url: "https://burst.shopifycdn.com/photos/flatlay-iron-skillet-with-meat-and-other-food.jpg?width=1200&format=pjpg&exif=1&iptc=1",
-    },
-    {
-      type: "square",
-      url: "https://equitek.ca/wp-content/uploads/2020/10/Pizza-Hut-logo.jpg",
-    },
-    {
-      type: "square",
-      url: "https://dcassetcdn.com/design_img/3699781/809346/22123233/cpyfs55fdepsayrhkd5sqkm2m5_image.jpg",
-    },
-    {
-      type: "rect",
-      url: "https://media.istockphoto.com/id/1220017909/photo/top-view-table-full-of-food.jpg?s=170667a&w=0&k=20&c=aqS9rBQtGzmcOP4ddNKYTU7h8gyG3Y3KVhsh1hlQd-g=",
-    },
-    {
-      type: "square",
-      url: "https://www.vetrina.toscana.it/wp-content/uploads/2021/11/Pokeria-by-nima-b9d230a3.jpg",
-    },
-    {
-      type: "square",
-      url: "https://cdn.mos.cms.futurecdn.net/8tEvBrHEeMTTBt26SiniHM-1200-80.jpg",
-    },
-    {
-      type: "square",
-      url: "https://ristoratori.mycia.it/images/logos/256.jpg",
-    },
-  ],
-};
+const sliderImages = [
+  {
+    type: "rect",
+    url: "https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg",
+  },
+  {
+    type: "square",
+    url: "https://www.diregiovani.it/wp-content/uploads/2017/02/logo-McDonald%E2%80%99s.jpg",
+  },
+  {
+    type: "square",
+    url: "https://media.cdnandroid.com/item_images/600470/imagen-old-wild-west-0ori.jpg",
+  },
+  {
+    type: "square",
+    url: "https://inscandicci.it/wp-content/uploads/2019/03/Runner-Pizza-evidenza.jpg",
+  },
+  {
+    type: "rect",
+    url: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg",
+  },
+  {
+    type: "rect",
+    url: "https://burst.shopifycdn.com/photos/flatlay-iron-skillet-with-meat-and-other-food.jpg?width=1200&format=pjpg&exif=1&iptc=1",
+  },
+  {
+    type: "square",
+    url: "https://equitek.ca/wp-content/uploads/2020/10/Pizza-Hut-logo.jpg",
+  },
+  {
+    type: "square",
+    url: "https://dcassetcdn.com/design_img/3699781/809346/22123233/cpyfs55fdepsayrhkd5sqkm2m5_image.jpg",
+  },
+  {
+    type: "rect",
+    url: "https://media.istockphoto.com/id/1220017909/photo/top-view-table-full-of-food.jpg?s=170667a&w=0&k=20&c=aqS9rBQtGzmcOP4ddNKYTU7h8gyG3Y3KVhsh1hlQd-g=",
+  },
+  {
+    type: "square",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyVowqYZqk3tx6qVWxan_vD5f3MagIHMtJIKEl8WbflQ&s",
+  },
+  {
+    type: "square",
+    url: "https://cdn.mos.cms.futurecdn.net/8tEvBrHEeMTTBt26SiniHM-1200-80.jpg",
+  },
+  {
+    type: "square",
+    url: "https://ristoratori.mycia.it/images/logos/256.jpg",
+  },
+];
 
 export { sliderImages };
