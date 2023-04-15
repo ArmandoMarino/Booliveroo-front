2ac9996790532077f974b4ee090478dae98fc036
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-04-15 11:24:41 +0200

    install and import plugin Braintree

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import store from "./store"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
+import vueBraintree from 'vue-braintree'
+
 // TODO import AppLoader from './components/AppLoader.vue';
 import { router } from './router';
 
@@ -12,5 +14,7 @@ const app = createApp(App);
 app.use(router);
 
 app.use(store);
+
+app.use(vueBraintree);
 
 app.mount('#app');
