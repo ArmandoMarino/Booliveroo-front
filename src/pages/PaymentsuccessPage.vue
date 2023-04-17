c10484bbc5449b1633ad8a66ac45f681395690d1
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-04-17 18:00:47 +0200

    fix height payment-success/failed

--- src/pages/PaymentsuccessPage.vue
+++ src/pages/PaymentsuccessPage.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex flex-column align-items-center justify-content-center vh-100">
+    <div id="payment-success" class="d-flex flex-column align-items-center justify-content-center">
         <h4 class="mb-5">
             <i class="me-3 fa-solid fa-2xl text-success fa-circle-check fa-bounce"></i>
             Pagamento avvenuto con successo
@@ -15,3 +15,9 @@ export default {
         </router-link>
     </div>
 </template>
+
+<style lang="scss" scoped>
+#payment-success {
+    min-height: 400px;
+}
+</style>
