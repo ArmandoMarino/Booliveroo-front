d3fbe6f203c730a2137a9cb29c048c0ddf7ddfd3
DarioGiusino <dario.giusino@gmail.com>
2023-04-17 16:28:38 +0200

    changed name to pay button

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -67,7 +67,7 @@ export default {
 
 <template>
     <div id="dropin-container"></div>
-    <button id="submit-button" class="button button--small button--green">Purchase</button>
+    <button id="submit-button" class="button button--small button--green">Paga adesso</button>
 </template>
 
 <style scoped lang="scss">
