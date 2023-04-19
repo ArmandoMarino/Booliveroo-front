a14061b2c881941a46574d9a2f8ad3069621b649
DarioGiusino <dario.giusino@gmail.com>
2023-04-19 11:01:50 +0200

    removed loading on dropin error

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -36,6 +36,7 @@ export default {
                         if (err) {
                             console.error(err);
                             console.log('sono qui');
+                            this.isLoading = false;
                             return;
                         }
 
